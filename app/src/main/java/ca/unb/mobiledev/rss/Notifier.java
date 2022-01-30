36c527f9aadaf494015c5f8db0947626e246052c
Martin McArthur <martin.mcarthur@qps.nl>
2022-01-30 10:01:21 -0400

    Simplified custom notification class

--- app/src/main/java/ca/unb/mobiledev/rss/Notifier.java
+++ app/src/main/java/ca/unb/mobiledev/rss/Notifier.java
@@ -8,15 +8,16 @@ import androidx.core.app.NotificationCompat;
 public class Notifier
 {
     NotificationCompat.Builder m_builder;
-    Context m_context;
+    NotificationManager m_manager;
+
+    int m_lastUsedNotificationId = 0;
 
     public Notifier(Context activityContext)
     {
-        m_context = activityContext;
         m_builder = new NotificationCompat.Builder(activityContext);
         m_builder.setSmallIcon(R.drawable.common_google_signin_btn_icon_light);
-        //builder.setContentTitle("New Items posted to your rss feed");
-        //builder.setContentTitle("The Item XXX has been added - Maybe I should also update you on price changes?");
+
+        m_manager = (NotificationManager) activityContext.getSystemService(Context.NOTIFICATION_SERVICE);
     }
 
     public void postNotification(String titleText, String infoText)
@@ -24,9 +25,7 @@ public class Notifier
         m_builder.setContentTitle(titleText);
         m_builder.setContentText(infoText);
 
-        NotificationManager notificationManager = (NotificationManager)m_context.getSystemService(Context.NOTIFICATION_SERVICE);
-
-        int notificationID = 0; //Allows you to update the notification later on.
-        notificationManager.notify(notificationID, m_builder.build());
+        int notificationID = m_lastUsedNotificationId += 1; //Allows you to update the notification later on.
+        m_manager.notify(notificationID, m_builder.build());
     }
 }
