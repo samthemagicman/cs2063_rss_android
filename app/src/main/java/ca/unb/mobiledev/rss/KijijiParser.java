388f51939d938b9b2e5af764dd399396e1837068
Martin McArthur <martin.mcarthur@qps.nl>
2022-02-03 21:19:24 -0400

    Move volley class get rssfeed data to the kijijiparser class.

--- app/src/main/java/ca/unb/mobiledev/rss/KijijiParser.java
+++ app/src/main/java/ca/unb/mobiledev/rss/KijijiParser.java
@@ -1,7 +1,17 @@
 package ca.unb.mobiledev.rss;
 
+import android.content.Context;
 import android.graphics.Bitmap;
+import android.media.Image;
+import android.util.Log;
 import android.util.Xml;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
@@ -17,19 +27,75 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Date;
 
-public class KijijiParser
+interface ParsingListener
 {
-    public static KijijiItemPackage getItemsFromKijiji(String url)
+    void onParsingCompleted(KijijiItemPackage pack);
+}
+
+public class KijijiParser implements Response.Listener, Response.ErrorListener, OnTaskCompleted
+{
+    Context m_context;
+    ParsingListener m_listener;
+    KijijiItemPackage m_package;
+
+
+    public KijijiParser(Context context, ParsingListener listener) {
+        this.m_context = context;
+        this.m_listener = listener;
+
+    }
+
+    public void getRssFeed(String url)
     {
-        KijijiItemPackage pack = new KijijiItemPackage(url);
-        try {
-            pack.ParseRssFeed();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (XmlPullParserException e) {
+        RequestQueue queue = Volley.newRequestQueue(m_context);
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, this, this);
+        queue.add(stringRequest);
+    }
+
+    public void getItemsFromKijiji(String rssFeedXML)
+    {
+        m_package = new KijijiItemPackage(rssFeedXML);
+        try
+        {
+            m_package.ParseRssFeed();
+            GetImages();
+        }
+        catch (IOException e)
+        {
             e.printStackTrace();
         }
+        catch (XmlPullParserException e)
+        {
+            e.printStackTrace();
+        }
+    }
 
-        return pack;
+    private void GetImages()
+    {
+        for(int i = 0; i < m_package.items.size(); i++)
+        {
+            BaseItem item = m_package.items.get(i);
+            if(item.bitmapImage != null) continue;
+
+            ImageParserUtilities.RetrieveImageTask task = new ImageParserUtilities.RetrieveImageTask(item, this);
+            task.execute(item.bitmapLink);
+        }
+    }
+
+    @Override
+    public void onErrorResponse(VolleyError error) {
+        Log.d("KijijiParser", "Volley Comms Error");
+    }
+
+    @Override
+    public void onResponse(Object response)
+    {
+        getItemsFromKijiji( (String) response);
+    }
+
+    @Override
+    public void onImageDownloadCompleted()
+    {
+        m_listener.onParsingCompleted(m_package);
     }
 }
--- app/src/main/java/ca/unb/mobiledev/rss/ListingActivity.java
+++ app/src/main/java/ca/unb/mobiledev/rss/ListingActivity.java
@@ -20,7 +20,10 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.widget.Toast;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -33,14 +36,14 @@ import java.util.ArrayList;
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class ListingActivity extends AppCompatActivity implements OnTaskCompleted, LocationListener, Response.Listener, Response.ErrorListener {
+public class ListingActivity extends AppCompatActivity implements OnTaskCompleted, LocationListener, ParsingListener {
 
-    private ListAdapter listAdapter;
+    private ListAdapter m_listAdapter;
     private ArrayList<String> m_rssUrlList;
     private BaseItemsPackage m_currentPackage;
-    private LocationManager locationManager;
+    private LocationManager m_locationManager;
 
-    private Timer updateRssTimer = null;
+    private Timer m_updateRssTimer = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,23 +66,9 @@ public class ListingActivity extends AppCompatActivity implements OnTaskComplete
     protected void onStart() {
         super.onStart();
 
-        //ParseTheRssDataFromUrl(m_rssUrlList.get(0));
+        startRssParsingTimer(0, 10000);
 
-        // TODO: - Move this timer into its on method so we can start and stop it.
-        //Lets check for updates on the RssStream and then notify.
-        // Do task over and over
-        int delay = 0;
-        int period = 10000; //10s
-        updateRssTimer = new Timer();
-        updateRssTimer.scheduleAtFixedRate(new TimerTask() {
-            @Override
-            public void run() {
-                ParseTheRssDataFromUrl(m_rssUrlList.get(0));
-            }
-        }, delay, period);
-
-        // Get the current location of the device
-        locationManager = (LocationManager) getSystemService(this.LOCATION_SERVICE);
+        startLocationManager();
 
         boolean accessFineLocation = ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
         boolean accessCourseLocation = ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED;
@@ -90,19 +79,105 @@ public class ListingActivity extends AppCompatActivity implements OnTaskComplete
         }
         else
         {
-           DoRunGetLocationLoop();
+           startLocationServicesLoop();
         }
     }
 
-    private void DoRunGetLocationLoop()
+    @Override
+    protected void onStop() {
+        super.onStop();
+        stopRssParsingTimer();
+        //TODO: - stop location services
+        SaveViewingHistoryItems();
+    }
+
+    // INFO: - The following overrides are for the option menu in the toolbar.
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.list_menu, menu);
+
+        Object obj = (Object) findViewById(R.id.menu_mark_all_as_new_button);
+
+        MenuItem spinnerMenuItem =  menu.findItem(R.id.menu_spinner);
+        Spinner spinner = (Spinner) spinnerMenuItem.getActionView();
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.planets_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+//        spinner.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l)
+//            {
+//
+//            }
+//        });
+
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item)
     {
+        int id = item.getItemId();
+
+        if(id == R.id.menu_mark_all_as_viewed_button || id == R.id.menu_mark_all_as_new_button)
+        {
+            boolean showIndicator = id == R.id.menu_mark_all_as_new_button;
+            for (int i = 0; i < m_currentPackage.items.size(); i++)
+            {
+                m_currentPackage.items.get(i).showIndicator = showIndicator;
+            }
+
+            // FIXME: - Be smarter!
+            SaveViewingHistoryItems();
+            m_listAdapter.notifyDataSetChanged();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void initRecyclerView()
+    {
+        RecyclerView view = findViewById(R.id.recyclerView);
+        view.addItemDecoration(new DividerItemDecoration(view.getContext(), DividerItemDecoration.VERTICAL));
+        m_listAdapter = new ListAdapter(m_currentPackage, this);
+        view.setAdapter(m_listAdapter);
+        view.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    // INFO: - Location Manager (GPS) signal overrides.
+    // The following functions are the listeners for when the gps
+    // location is updated.
+    private void startLocationManager()
+    {
+        m_locationManager = (LocationManager) getSystemService(this.LOCATION_SERVICE);
+
+        boolean accessFineLocation = ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+        boolean accessCourseLocation = ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+
+        if (!accessFineLocation && !accessCourseLocation)
+        {
+            ActivityCompat.requestPermissions((Activity) this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 101);
+        }
+        else
+        {
+            startLocationServicesLoop();
+        }
+    }
+
+    private void startLocationServicesLoop()
+    {
+        if(m_locationManager == null) return;
+
         boolean accessFineLocation = ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
         boolean accessCourseLocation = ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED;
 
         if(accessFineLocation && accessCourseLocation)
         {
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,1000L,500.0f, this);
-            Location loc = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            m_locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,1000L,500.0f, this);
+            Location loc = m_locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
         }
     }
 
@@ -112,94 +187,20 @@ public class ListingActivity extends AppCompatActivity implements OnTaskComplete
     {
         if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
         {
-            DoRunGetLocationLoop();
+            startLocationServicesLoop();
         }
-        else
-        {
-            //TODO: - Inform User.
-        }
+
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 
     @Override
-    protected void onStop() {
-        super.onStop();
-        updateRssTimer.cancel();
-        updateRssTimer = null;
-        SaveViewingHistoryItems();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.list_menu, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item)
-    {
-        int id = item.getItemId();
-
-        //FIXME: - stop and start timer here. This might step all over itself.
-
-        if(id == R.id.menu_mark_all_as_viewed_button || id == R.id.menu_mark_all_as_new_button)
-        {
-            boolean showIndicator = (id == R.id.menu_mark_all_as_new_button) ? true : false;
-            for (int i = 0; i < m_currentPackage.items.size(); i++)
-            {
-                m_currentPackage.items.get(i).showIndicator = showIndicator;
-            }
-
-            // FIXME: - Be smarter!
-            SaveViewingHistoryItems();
-            listAdapter.notifyDataSetChanged();
-        }
-
-        return super.onOptionsItemSelected(item);
-
-    }
-
-    private void initRecyclerView()
-    {
-        RecyclerView view = findViewById(R.id.recyclerView);
-        view.addItemDecoration(new DividerItemDecoration(view.getContext(), DividerItemDecoration.VERTICAL));
-        listAdapter = new ListAdapter(m_currentPackage, this);
-        view.setAdapter(listAdapter);
-        view.setLayoutManager(new LinearLayoutManager(this));
-    }
-
-
-    private void RequestImagesInBackground(BaseItemsPackage rssPackage)
-    {
-        // FIXME: - Need to get images somehow... someway... who knows how? Here or back in the kijiji parsing area.
-        // Get the images in the background because they have not been downloaded and processed yet.
-        for (BaseItem item : rssPackage.items) {
-            if (item.bitmapImage != null) continue; //Dont try to load the image for something that already exists
-            ImageParserUtilities.RetrieveImageTask task = new ImageParserUtilities.RetrieveImageTask(item, this);
-            task.execute(item.bitmapLink);
-        }
-    }
-
-    // When the images have been loaded from the webpage,
-    // we need to update our recycler view - this is how we do it.
-    @Override
-    public void onImageDownloadCompleted()
-    {
-        //TODO: - Return model from this async task. Find the matching object in the list
-        // then update the individual componenet, not the whole list.
-        listAdapter.notifyDataSetChanged();
-    }
-
-    // Location Listeners
-    @Override
     public void onLocationChanged(@NonNull Location location)
     {
         try {
-            if(listAdapter == null) return;
+            if(m_listAdapter == null) return;
             if(location == null) return;
 
-            listAdapter.updateCurrentDeviceLocation(location);
+            m_listAdapter.updateCurrentDeviceLocation(location);
         }
         catch(Exception e)
         {
@@ -214,71 +215,61 @@ public class ListingActivity extends AppCompatActivity implements OnTaskComplete
         // maybe we should be doing something with this?
     }
 
-    // Volley Listeners
+    // INFO - VOLLEY
+    // The following  are the volley callbacks.
+    // Volley is used so we can get the xml data from the RSS server.
+    // Abstracting this stuff out would be a considerable amount of work and crazy becasue
+    // no one else is using this code.
     private void ParseTheRssDataFromUrl(String url)
     {
-        RequestQueue queue = Volley.newRequestQueue(this);
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, this, this);
-        queue.add(stringRequest);
+        KijijiParser parser = new KijijiParser(this, this);
+        parser.getRssFeed(url);
     }
 
     @Override
-    public void onResponse(Object response)
+    public void onParsingCompleted(KijijiItemPackage pack)
     {
-        String rssFeed = (String) response;
-        // TODO: - do this in background or we get small delay when counting for updates
-        try{
-            BaseItemsPackage newPackage = KijijiParser.getItemsFromKijiji(rssFeed);
-            RequestImagesInBackground(newPackage);
-
-            if(m_currentPackage == null)
-            {
-                m_currentPackage = newPackage;
-            }
-            else
-            {
-                // Copy over hasViewd and new booleans.
-                BaseItemsPackage.FeedUpdateInfo updateInfo = newPackage.UpdateFromAnotherPackage(m_currentPackage);
-                m_currentPackage = newPackage;
-
-                if(updateInfo.hasUpdates())
-                {
-                    Log.d("NEW ITEMS", "NEW ITEMS EMIT NOTIFICATION");
-
-                    //FIXME: - Create string with updates vs new Items
-                    String str = "";
-                    for(BaseItem item: m_currentPackage.items)
-                    {
-                        if(item.showIndicator)
-                            str = str + item.title + "\n";
-                    }
-
-                    DoNotification("New items are available on your RSS feed.");
-                }
-            }
-
-            ApplyViewHistoryItemContentToCurrentContent();
-            listAdapter.setData(m_currentPackage);
-            listAdapter.notifyDataSetChanged();
-
-
-        }
-        catch(Exception e)
+        if(m_currentPackage == null)
         {
-            //TODO: - Handle correctly with meesage to user.
-            e.printStackTrace();
+            m_currentPackage = pack;
         }
+        else
+        {
+            BaseItemsPackage.FeedUpdateInfo updateInfo = pack.UpdateFromAnotherPackage(m_currentPackage);
+            m_currentPackage = pack;
+
+            if(updateInfo.hasUpdates())
+            {
+                DoNotification("New Items Posted!");
+            }
+        }
+
+        ApplyViewHistoryItemContentToCurrentContent();
+        m_listAdapter.setData(m_currentPackage);
+        m_listAdapter.notifyDataSetChanged();
     }
 
-    @Override
-    public void onErrorResponse(VolleyError error)
+
+    // INFO: - Methods used for the class
+
+
+    private void startRssParsingTimer(int delay, int period)
     {
-        Log.d("VolleyError: " , error.toString());
-        //TODO: - do something here like show error text in View
+        m_updateRssTimer = new Timer();
+        m_updateRssTimer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                ParseTheRssDataFromUrl(m_rssUrlList.get(0));
+            }
+        }, delay, period);
+    }
+
+    private void stopRssParsingTimer()
+    {
+        m_updateRssTimer.cancel();
     }
 
 
-    // Sends system notification to user with info text.
     private void DoNotification(String infoText)
     {
         Notifier notify = new Notifier(this);
@@ -336,4 +327,5 @@ public class ListingActivity extends AppCompatActivity implements OnTaskComplete
 
         return editor.commit();
     }
+
 }
