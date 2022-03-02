db5463c967557ed62df37a7289a03b1ebf70d681
sam <samuelssalfi@gmail.com>
2022-03-02 15:27:02 -0400

    Added cancel button functionality

--- app/src/main/java/ca/unb/mobiledev/rss/WebviewSearch.java
+++ app/src/main/java/ca/unb/mobiledev/rss/WebviewSearch.java
@@ -47,6 +47,13 @@ public class WebviewSearch extends AppCompatActivity {
             }
         });
 
+        findViewById((R.id.cancel)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                WebviewSearch.super.onBackPressed();
+            }
+        });
+
 
         mainWebview =  findViewById(R.id.webview);
         WebViewClient webViewClient = new WebViewClient();
